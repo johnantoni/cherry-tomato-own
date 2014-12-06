ac52651d077a746b6dba37948c64d2d0a5106bd8
John Griffiths <john@uxgent.co>
2014-12-06 12:04:20 -0500

    re-organise files.

---
+++ client/views/pomodoros/pomodoros_list.js
@@ -0,0 +1,25 @@
+Template.pomodorosList.helpers({
+  totalPomodoros: function() {
+    return Pomodoros.find().fetch().length;
+  },
+  allPomodoros: function () {
+    return Pomodoros.find({}, {sort: {startDate: -1}});
+  }
+});
+
+Template.pomodorosList.events({
+  'submit #new-pomodoro' : function (e) {
+    e.preventDefault();
+
+    var pomodoro = {
+      startDate: new Date(),
+      goal: e.target.goal.value,
+    };
+
+    Pomodoros.insert(pomodoro);
+  },
+  'click .delete' : function (e) {
+    e.preventDefault();
+    Pomodoros.remove(this._id);
+  }
+});
