c245f16cab4ad03c6fce9df1fbfd79328fd21258
John Griffiths <john@uxgent.co>
2014-12-07 14:08:34 -0500

    desktop notifications.

--- client/main.js
+++ client/main.js
@@ -2,9 +2,36 @@ Accounts.ui.config({
   passwordSignupFields: 'USERNAME_AND_EMAIL'
 });
 
+
+notifyMe = function() {
+  // Let's check if the browser supports notifications
+  if (!("Notification" in window)) {
+    alert("This browser does not support desktop notification");
+  }
+
+  // Let's check if the user is okay to get some notification
+  else if (Notification.permission === "granted") {
+    // If it's okay let's create a notification
+    var notification = new Notification("Hi there!");
+  }
+
+  // Otherwise, we need to ask the user for permission
+  // Note, Chrome does not implement the permission static property
+  // So we have to check for NOT 'denied' instead of 'default'
+  else if (Notification.permission !== 'denied') {
+    Notification.requestPermission(function (permission) {
+      // If the user is okay, let's create a notification
+      if (permission === "granted") {
+        var notification = new Notification("Hi there!");
+      }
+    });
+  }
+};
+
 title = function () {
   if (Meteor.user()) {
     document.title = formattedRemaining(currentPomodoro(Meteor.user())) ;
+    // var notification = new Notification(document.title);
   }
 };
 
